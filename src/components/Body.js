d8841fcaa32d78289df6f4ab1b546432b94076d5
adit-bala <adityarbala@gmail.com>
2022-05-26 17:42:46 -0400

    styling

--- src/components/Body.js
+++ src/components/Body.js
@@ -19,17 +19,26 @@ export default function Body() {
         </div>
         <div className="container">
           <div className="week-div">
+            <p className="current-week"> week 1: 6/21 - 6/24 </p>
             <div className="wrapper">
               <p className="week">
                 lab 00: getting started (optional; no session)
               </p>
-              <button> assignment </button>
+              <button className="button"> assignment </button>
+              <button className="button"> solution </button>
             </div>
             <div className="wrapper">
               <p className="week"> disc 00: getting started </p>
-              <button> slides </button>
+              <button className="button"> assignment </button>
+              <button className="button"> solution </button>
+            </div>
+            <div className="wrapper">
+              <p className="week"> slides 00: </p>
+              <button className="button slides"> html </button>
+              <button className="button slides"> pdf </button>
             </div>
           </div>
+          
         </div>
       </div>
     </div>
