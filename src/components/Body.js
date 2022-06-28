f56ad1f485012f7bf574b89c34c2375c4297483d
adit-bala <adityarbala@gmail.com>
2022-06-27 17:48:27 -0700

    updates for disc02

--- src/components/Body.js
+++ src/components/Body.js
@@ -19,7 +19,56 @@ export default function Body() {
         </div>
         <div className="container">
           <div className="week-div">
-            <p className="current-week"> week 1: 6/21 - 6/24 </p>
+            <p className="current-week"> <b>week 2: 6/27 - 7/1</b> </p>
+            <div className="wrapper">
+              <p className="week">lab 01: Variables and Functions, Control </p>
+              <a className="button slides"> attendance </a>
+              <a className="button"> assignment </a>
+              <a className="button"> solution </a>
+            </div>
+            <div className="wrapper">
+              <p className="week"> disc 02: Environment Diagrams, HOF</p>
+              <a
+                href="https://tinyurl.com/adit-disc02"
+                target="_blank"
+                className="button slides"
+                rel="noreferrer"
+              >
+                {" "}
+                attendance{" "}
+              </a>
+              <a
+                href="https://cs61a.org/disc/disc02/"
+                target="_blank"
+                className="button slides"
+                rel="noreferrer"
+              >
+                {" "}
+                assignment{" "}
+              </a>
+              <a className="button slides"> solution </a>
+            </div>
+            <div className="wrapper">
+              <p className="week"> slides 02: </p>
+              <a
+                href="https://slides.aditbala.com/su22/disc02"
+                target="_blank"
+                className="button slides"
+                rel="noreferrer"
+              >
+                html
+              </a>
+              <a
+                href="https://slides.aditbala.com/su22/disc02.pdf"
+                target="_blank"
+                className="button slides"
+                rel="noreferrer"
+              >
+                pdf
+              </a>
+            </div>
+            <br></br>
+            <p className="current-week"> <b>week 1: 6/21 - 6/24</b> </p>
             <div className="wrapper">
               <p className="week">lab 00: getting started</p>
               <a
@@ -84,18 +133,33 @@ export default function Body() {
             <br></br>
             <div className="wrapper">
               <p className="week">lab 01: Variables and Functions, Control </p>
-              <a href="https://tinyurl.com/adit-lab01"
+              <a
+                href="https://tinyurl.com/adit-lab01"
                 target="_blank"
                 className="button slides"
-                rel="noreferrer"> attendance </a>
-              <a href="https://tinyurl.com/adit-study"
+                rel="noreferrer"
+              >
+                {" "}
+                attendance{" "}
+              </a>
+              <a
+                href="https://tinyurl.com/adit-study"
                 target="_blank"
                 className="button slides"
-                rel="noreferrer"> study group </a>
-              <a href="https://cs61a.org/lab/lab01/"
+                rel="noreferrer"
+              >
+                {" "}
+                study group{" "}
+              </a>
+              <a
+                href="https://cs61a.org/lab/lab01/"
                 target="_blank"
                 className="button"
-                rel="noreferrer"> assignment </a>
+                rel="noreferrer"
+              >
+                {" "}
+                assignment{" "}
+              </a>
               <a className="button"> solution </a>
             </div>
             <div className="wrapper">
@@ -121,10 +185,15 @@ export default function Body() {
                 {" "}
                 assignment{" "}
               </a>
-              <a href="https://cs61a.org/disc/sol-disc01/"
+              <a
+                href="https://cs61a.org/disc/sol-disc01/"
                 target="_blank"
                 className="button slides"
-                rel="noreferrer"> solution </a>
+                rel="noreferrer"
+              >
+                {" "}
+                solution{" "}
+              </a>
             </div>
             <div className="wrapper">
               <p className="week"> slides 01: </p>
