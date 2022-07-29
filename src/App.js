2517b33eb86cdcbb4291689fd7c74ca4e76fc97e
adit-bala <adityarbala@gmail.com>
2022-07-28 21:11:56 -0700

    FINALLY FIX STUPID 404 ERROR

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Main from "./components/Main";
 import Piazza from "./components/Piazza";
 import "./App.css";
