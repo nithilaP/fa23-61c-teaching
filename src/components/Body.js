b13ab82c71c764bf479002b9d90acf9567ba645f
adit-bala <adityarbala@gmail.com>
2022-09-05 07:40:35 -0700

    init , release sols for ,

--- src/components/Body.js
+++ src/components/Body.js
@@ -13,6 +13,22 @@ export default function Body() {
         </div>
         <div className="container">
           <div className="week-div">
+          <Week
+              num="03"
+              mStart="9"
+              dStart="2"
+              mEnd="9"
+              dEnd="9"
+              content={[
+                [
+                  "02",
+                  "Higher-Order Functions, Lambda Expressions",
+                  "Environment Diagrams, Higher-Order Functions",
+                  [false, true],
+                  [false, false],
+                ],
+              ]}
+            />
             <Week
               num="02"
               mStart="8"
@@ -25,7 +41,7 @@ export default function Body() {
                   "Functions, Control",
                   "Functions, Control, Environment Diagram",
                   [true, true],
-                  [false, false],
+                  [true, true],
                 ],
               ]}
             />
@@ -41,7 +57,7 @@ export default function Body() {
                   "Getting Started",
                   "Getting Started",
                   [true, true],
-                  [true, false],
+                  [true, true],
                 ],
               ]}
             />
