4c55d70dfffa9011e13022c10aea4f882c4af4a3
adit-bala <adityarbala@gmail.com>
2022-09-14 18:20:33 -0700

    release `disc03`

--- src/components/Body.js
+++ src/components/Body.js
@@ -24,7 +24,7 @@ export default function Body() {
                   "03",
                   "Midterm Review (Optional)",
                   "Recursion",
-                  [false, true],
+                  [true, true],
                   [false, true],
                 ],
               ]}
