f68e1445244ed87452593a84176126ef8762cf0f
adit-bala <adityarbala@gmail.com>
2023-05-28 09:19:46 -0700

    mv `61a` to `/61a`

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Main from "./components/Main";
+import A from "./components/A";
+import C from "./components/C";
 import "./App.css";
 
 function App() {
   return (
     <Routes>
-      <Route path="" element={<Main />} />
+      <Route path="" element={<C />} />
+      <Route path="/61a" element={<A />} />
     </Routes>
   );
 }
